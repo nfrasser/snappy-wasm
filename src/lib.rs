39d30fe580f8c8ce5f3a4cf18934f11430981aa9
Nick Frasser <nfrasser@gmail.com>
2022-04-06 09:34:20 -0400

    Improved error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod utils;
 
-use wasm_bindgen::prelude::*;
-use std::io::copy;
 use snap;
+use std::io;
+use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,54 +11,54 @@ use snap;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn compress(input: &[u8]) -> Option<Box<[u8]>> {
+pub fn compress(input: &str) -> Result<Box<[u8]>, JsError> {
+    compress_raw(input.as_bytes())
+}
+
+#[wasm_bindgen]
+pub fn compress_raw(input: &[u8]) -> Result<Box<[u8]>, JsError> {
     let mut iref = input;
     let mut output: Vec<u8> = Vec::new();
     let mut writer = snap::write::FrameEncoder::new(&mut output);
-    if let Ok(_) = copy(&mut iref, &mut writer) {
-        drop(writer);
-        Some(output.into_boxed_slice())
-    } else {
-        None
-    }
-}
-
-
-#[wasm_bindgen]
-pub fn compress_str(input: &str) -> Option<Box<[u8]>> {
-    compress(input.as_bytes())
-}
-
-
-#[wasm_bindgen]
-pub fn decompress(input: &[u8]) -> Option<Box<[u8]>> {
-    if let Some(output) = util::decompress(input) {
-        Some(output.into_boxed_slice())
-    } else {
-        None
-    }
-}
-
-#[wasm_bindgen]
-pub fn decompress_str(input: &[u8]) -> Result<String, JsValue> {
-    if let Some(decompressed) = util::decompress(input) {
-        match String::from_utf8(decompressed) {
-            Ok(s) => Ok(s),
-            Err(_) => Err(JsValue::from("Invalid string"))
+    match io::copy(&mut iref, &mut writer) {
+        Ok(_) => {
+            drop(writer);
+            Ok(output.into_boxed_slice())
         }
-    } else {
-        Err(JsValue::from("Could not decompress given data"))
+        Err(err) => Err(util::handle_err(err))
+    }
+}
+
+#[wasm_bindgen]
+pub fn decompress(input: &[u8]) -> Result<String, JsError> {
+    match util::decompress(input) {
+        Ok(decompressed) =>  match String::from_utf8(decompressed) {
+            Ok(s) => Ok(s),
+            Err(_) => Err(JsError::new("Cannot decompress invalid UTF-8 string")),
+        }
+        Err(err) => Err(util::handle_err(err))
+    }
+}
+
+#[wasm_bindgen]
+pub fn decompress_raw(input: &[u8]) -> Result<Box<[u8]>, JsError> {
+    match util::decompress(input) {
+        Ok(output) => Ok(output.into_boxed_slice()),
+        Err(err) => Err(util::handle_err(err))
     }
 }
 
 mod util {
-    pub fn decompress(input: &[u8]) -> Option<Vec<u8>> {
+    use std::io;
+
+    pub fn decompress(input: &[u8]) -> io::Result<Vec<u8>> {
         let mut reader = snap::read::FrameDecoder::new(input);
         let mut output: Vec<u8> = Vec::new();
-        if let Ok(_) = super::copy(&mut reader, &mut output) {
-            Some(output)
-        } else {
-            None
-        }
+        io::copy(&mut reader, &mut output)?;
+        Ok(output)
+    }
+
+    pub fn handle_err(err: io::Error) -> super::JsError {
+        super::JsError::new(err.to_string().as_str())
     }
 }
--- tests/web.rs
+++ tests/web.rs
@@ -3,8 +3,8 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
-use wasm_bindgen_test::*;
 use snappy_wasm;
+use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
@@ -12,28 +12,36 @@ const TEST_STRING: &str = "Hello World Hello World Hello World Hello World Hello
 
 #[wasm_bindgen_test]
 fn test_compress() {
-    let compressed = snappy_wasm::compress(TEST_STRING.as_bytes());
+    let compressed = snappy_wasm::compress(TEST_STRING);
     assert!(compressed.is_some(), "Compressed some bytes")
 }
 
 #[wasm_bindgen_test]
-fn test_compress_str() {
-    let compressed = snappy_wasm::compress_str(TEST_STRING);
+fn test_compress_raw() {
+    let compressed = snappy_wasm::compress_raw(TEST_STRING.as_bytes());
     assert!(compressed.is_some(), "Compressed some bytes")
 }
 
 #[wasm_bindgen_test]
 fn test_decompress() {
-    let compressed = snappy_wasm::compress(TEST_STRING.as_bytes()).unwrap();
+    let compressed = snappy_wasm::compress(TEST_STRING).unwrap();
     let decompressed = snappy_wasm::decompress(compressed.as_ref());
-    assert!(decompressed.is_some(), "Decompressed some bytes");
-    assert_eq!(decompressed.unwrap().as_ref(), TEST_STRING.as_bytes(), "Decompressed original string");
+    assert!(decompressed.is_ok(), "Decompressed some bytes");
+    assert_eq!(
+        decompressed.unwrap(),
+        TEST_STRING,
+        "Decompressed original string"
+    );
 }
 
 #[wasm_bindgen_test]
-fn test_decompress_str() {
-    let compressed = snappy_wasm::compress_str(TEST_STRING).unwrap();
-    let decompressed = snappy_wasm::decompress_str(compressed.as_ref());
-    assert!(decompressed.is_ok(), "Decompressed some bytes");
-    assert_eq!(decompressed.unwrap(), TEST_STRING, "Decompressed original string");
+fn test_decompress_raw() {
+    let compressed = snappy_wasm::compress_raw(TEST_STRING.as_bytes()).unwrap();
+    let decompressed = snappy_wasm::decompress_raw(compressed.as_ref());
+    assert!(decompressed.is_some(), "Decompressed some bytes");
+    assert_eq!(
+        decompressed.unwrap().as_ref(),
+        TEST_STRING.as_bytes(),
+        "Decompressed original string"
+    );
 }
